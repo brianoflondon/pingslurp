a563af307617366342a0caf63ac5c6c745573956
BOL P51 <bol.p51@v4v.app>
2022-10-27 10:40:15 +0300

    podping class moved
    Signed-off-by: BOL P51 <bol.p51@v4v.app>

--- src/podping_hive/podping.py
+++ src/podping_hive/podping.py
@@ -1,10 +1,78 @@
-from pydantic import BaseModel
 from datetime import datetime
+import json
+from typing import Any, List, Literal
 
-class Podping(BaseModel):
-    block: int
+from pydantic import BaseModel, validator
+
+mediums = {
+    "podcast",
+    "music",
+    "video",
+    "film",
+    "audiobook",
+    "newsletter",
+    "blog",
+    "podcastL",
+    "musicL",
+    "videoL",
+    "filmL",
+    "audiobookL",
+    "newsletterL",
+    "blogL",
+}
+reasons = {"update", "live", "liveEnd", "newIRI"}
+
+
+def utf8len(s):
+    return len(s.encode("utf-8"))
+
+
+class HiveTrx(BaseModel):
+    trx_id: str
+    block_num: int
     timestamp: datetime
-    version: str
+    trx_num: int
+
+
+class PodpingMeta(BaseModel):
+    required_posting_auths: List[str]
+    json_size: int
     id: str
-    medium: str = None
-    reason: str = None
+
+
+class Podping(HiveTrx, PodpingMeta, BaseModel):
+    """Dataclass for on-chain podping schema"""
+
+    version: Literal["1.0"] = "1.0"
+    medium: str
+    reason: str
+    iris: List[str]
+
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        custom_json = json.loads(data["op"][1]["json"])
+        hive_trx = data
+        podping_meta = data["op"][1]
+        podping_meta["json_size"] = utf8len(data["op"][1]["json"])
+        super().__init__(**custom_json, **hive_trx, **podping_meta)
+
+    @validator("medium")
+    def medium_exists(cls, v):
+        """Make sure the given medium matches what's available"""
+        if v not in mediums:
+            raise ValueError(f"medium must be one of {str(', '.join(mediums))}")
+        return v
+
+    @validator("reason")
+    def reason_exists(cls, v):
+        """Make sure the given reason matches what's available"""
+        if v not in reasons:
+            raise ValueError(f"reason must be one of {str(', '.join(reasons))}")
+        return v
+
+    @validator("iris")
+    def iris_at_least_one_element(cls, v):
+        """Make sure the list contains at least one element"""
+        if len(v) == 0:
+            raise ValueError("iris must contain at least one element")
+
+        return v
--- src/podping_hive/scanner.py
+++ src/podping_hive/scanner.py
@@ -1,11 +1,14 @@
 import asyncio
+import enum
 import inspect
+import json
 import logging
 import os
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 from functools import wraps
 from timeit import default_timer as timer
-from typing import List, Optional
+from typing import Any, List, Literal, Optional
 
 import backoff
 from lighthive.client import Client
@@ -13,26 +16,32 @@ from lighthive.datastructures import Operation
 from lighthive.exceptions import RPCNodeException
 from lighthive.helpers.account import VOTING_MANA_REGENERATION_IN_SECONDS
 from lighthive.node_picker import compare_nodes
-from pydantic import BaseModel, Field
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
+from pydantic import BaseModel, Field, validator
 
 from podping_hive.hive import get_client, listen_for_custom_json_operations
+from podping_hive.podping import Podping
 
+DB_CONNECTION = "mongodb://localhost:27017"
 
-class HiveTrx(BaseModel):
-    trx_id: str = Field(alias="id")
-    block_num: int
-    trx_num: int
-    expired: bool
+def get_mongo_db(collection: str) -> AsyncIOMotorCollection:
+    """Returns the MongoDB"""
+    return AsyncIOMotorClient(DB_CONNECTION)["podping"][collection]
 
 
 async def main_loop():
     client = get_client()
     current_block = client.get_dynamic_global_properties()["head_block_number"]
+    database = get_mongo_db("all_podpings")
     async for post in listen_for_custom_json_operations(
-        condenser_api_client=client, start_block=current_block - 200
+        condenser_api_client=client, start_block=current_block - 2000
     ):
         if post["op"][1]["id"].startswith("pp_"):
-            logging.info(post)
+            podping = Podping.parse_obj(post)
+            data = podping.dict()
+            await database.insert_one(data)
+            logging.info(podping)
+            logging.info(datetime.utcnow() - podping.timestamp)
 
 
 if __name__ == "__main__":
